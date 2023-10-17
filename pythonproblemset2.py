cdf45076cd1f9a64feab5d734e9c37ab411468a5
pfb10 <pfb10@info10.cshl.edu>
2023-10-17 15:42:37 -0400

    adding first file for the python2 problems

---
+++ pythonproblemset2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+cavefish = 34
+if cavefish<20:
+	message = ("1.5 million years ago, Astyanax mexicanus diverged into 19 caves")
+	print(message)
+elif cavefish == 31:
+	message = ("1.5 million years ago, Astyanax mexicanus diverged into 31 caves")
+	print(message)
+else:
+	message = ("1.5 million years ago, Astyanax mexicanus diverged into 34 caves")
+	print(message)
+
+
