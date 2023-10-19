864b7fd2d3a2cc782d4b7b0f5c7dd48c54c7ef37
pfb10 <pfb10@info10.cshl.edu>
2023-10-19 17:21:18 -0400

    python2 problem set answers, update needed

---
+++ pf10p2problems.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+cavefish = 34
+if cavefish < 20:
+	message = ("1.5 million years ago, Astyanax mexicanus diverged into 19 caves")
+	print(message)
+if cavefish > 0:
+	message = ("number is positive")
+	print(message)
+if cavefish < 0:
+        message = ("number is negative")
+        print(message)
+
